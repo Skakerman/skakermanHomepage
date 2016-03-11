d1f9e41a956e36c7eb53d7cbb3547bad3c0e6892
Skakerman <scott.r.akerman@gmail.com>
2016-03-10 22:18:10 -0500

    cname

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function (grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    'gh-pages': {
+      options: {
+        base: 'build'
+      },
+      src: ['**/*']
+    },
+
+    copy: {
+      build: {
+        cwd: 'app',
+        src: ['**', '!**/*_test*', '!bower_components/**'],
+        dest: 'build',
+        expand: true
+      }
+    },
+
+    clean: {
+      build: {
+        src: ['build']
+      }
+    },
+
+    uglify: {
+      build: {
+        options: {
+          mangle: true,
+          banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        },
+        files: {
+          'build/app.min.js': ['app/**/*.js']
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-gh-pages');
+
+  // Default task(s).
+  grunt.registerTask('default', ['clean:build', 'copy']);
+
+};
